fc24c1e7f00f02844672f1a9fa1a4609d3e4b4e0
Ritesh Mali <63728550+Rityamali@users.noreply.github.com>
2024-03-19 12:20:46 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import TransactionDashboard from "./components/TransactionDashboard";
+import TransactionsBarChar from "./components/TransactionsBarChar";
+import TransctionsStatistics from "./components/TransctionsStatistics";
+
+function App() {
+return (
+  <>
+  <TransactionDashboard></TransactionDashboard>
+  <TransctionsStatistics></TransctionsStatistics>
+  <TransactionsBarChar></TransactionsBarChar>
+  </>
+  );
+}
+
+export default App;
